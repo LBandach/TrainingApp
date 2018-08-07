5025268e76f0cbe75e3a8944128d8820e2a0b6ac
BanLuk <bandachlukasz@gmail.com>
2018-08-07 13:07:20 +0200

    PlaySound(AVPlayer) implemented

--- Trainnig App/Controller/PlaySoundsViewController.swift
+++ Trainnig App/Controller/PlaySoundsViewController.swift
@@ -7,14 +7,75 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 class PlaySoundsViewController: UIViewController {
 
+    var soundPlayer: AVAudioPlayer?
+
+    @IBAction func Sound1(_ sender: UIButton) {
+        playSound(noteName: "note1.wav")
+    }
+    @IBAction func Sound2(_ sender: UIButton) {
+        playSound(noteName: "note2.wav")
+    }
+    @IBAction func Sound3(_ sender: UIButton) {
+        playSound(noteName: "note3.wav")
+    }
+    @IBAction func Sound4(_ sender: UIButton) {
+        playSound(noteName: "note4.wav")
+    }
+    @IBAction func Sound5(_ sender: UIButton) {
+        playSound(noteName: "note5.wav")
+    }
+    @IBAction func Sound6(_ sender: UIButton) {
+        playSound(noteName: "note6.wav")
+    }
+    @IBAction func Sound7(_ sender: UIButton) {
+        playSound(noteName: "note7.wav")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
 
+    
+    func playSound(noteName: String){
+        
+        let path = Bundle.main.path(forResource: noteName, ofType: nil)!
+        let url = URL(fileURLWithPath: path)
+        do {
+            soundPlayer = try AVAudioPlayer(contentsOf: url)
+            soundPlayer?.play()
+        } catch {
+            print("Error playing sound \(error)")
+        }
+        
+    }
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
